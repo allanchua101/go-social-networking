0fd7e3e59a59736b1e70678ec4dbe5a1f63c8055
Allan <allanchua.officefiles@gmail.com>
2019-02-28 23:03:24 +0800

    #19 Dockerized activity write daemon

---
+++ activity-write-daemon/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+)
+
+// main is the application's composition root.
+func main() {
+	log.Println("Write Daemon Starting....")
+
+	// forever := make(chan bool)
+
+	// log.Printf(" [*] Waiting for messages. To exit press CTRL+C")
+	// <-forever
+}

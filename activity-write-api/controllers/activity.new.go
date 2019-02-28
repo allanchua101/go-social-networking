6fe57bbedb4a390b8feae78253971e46aadf17f1
Brady Sullivan <brady@bsull.com>
2019-02-27 23:45:57 -0800

    Update activity.new.go
    We return early rather than follow nested if else statements.

--- activity-write-api/controllers/activity.new.go
+++ activity-write-api/controllers/activity.new.go
@@ -1,50 +1,51 @@
 package controllers
 
 import (
-	"os"
-	"fmt"
-	"activity-write-api/models"
-	"activity-write-api/validators"
-	"activity-write-api/emitters"
-	"github.com/gin-gonic/gin"
-	"github.com/twinj/uuid"
-	"encoding/json"
+    "activity-write-api/emitters"
+    "activity-write-api/models"
+    "activity-write-api/validators"
+    "encoding/json"
+    "fmt"
+    "github.com/gin-gonic/gin"
+    "github.com/twinj/uuid"
+    "os"
 )
 
 // HandleNewActivityRequest handles HTTP request for
 // pushing new activity data into the system.
 func HandleNewActivityRequest(c *gin.Context) {
-	input := new(models.Activity)
-	err := c.BindJSON(input)
-
-	if err != nil {
-		SendBadRequest(c, gin.H{
-			"payload": "Request cannot be parsed.",
-		})
-	} else {
-		validationIssues := validators.ValidateNewActivity(input)
-
-		if len(validationIssues) > 0 {
-			SendUnprocessableEntity(c, gin.H {
-				"payload": validationIssues,
-			})
-		} else {
-			input.ID = uuid.NewV4().String()
-
-			SendOK(c, gin.H{
-				"payload": input.ID,
-			})
-			
-			writeQueueName := os.Getenv("WRITE_API_QUEUE_NAME")
-			serializedData, serializeErr := json.Marshal(input)
-
-			if serializeErr == nil {
-				publishErr := emitters.PublishEvent(writeQueueName, string(serializedData))
-
-				fmt.Println("%s", publishErr)
-				// TODO: Circuit break to a log store.
-				// TODO: Trigger alarms about downtime + Send retry indicator to client side.
-			}
-		}
-	}
+    input := new(models.Activity)
+    err := c.BindJSON(input)
+    
+    if err != nil {
+        SendBadRequest(c, gin.H{
+            "payload": "Request cannot be parsed.",
+        })
+        return
+    }   
+    validationIssues := validators.ValidateNewActivity(input)
+        
+    if len(validationIssues) > 0 { 
+        SendUnprocessableEntity(c, gin.H{
+            "payload": validationIssues,
+        })  
+        return
+    }   
+     
+    input.ID = uuid.NewV4().String()
+            
+    SendOK(c, gin.H{
+        "payload": input.ID,
+    })      
+            
+    writeQueueName := os.Getenv("WRITE_API_QUEUE_NAME")
+    serializedData, serializeErr := json.Marshal(input)
+            
+    if serializeErr == nil {
+        publishErr := emitters.PublishEvent(writeQueueName, string(serializedData))
+                
+        fmt.Println("%s", publishErr)
+        // TODO: Circuit break to a log store.
+        // TODO: Trigger alarms about downtime + Send retry indicator to client side.
+    }
 }

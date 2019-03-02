5b87b0a1c69fbf2e35661bb28befe359d8258d86
Allan <allanchua.officefiles@gmail.com>
2019-03-02 16:49:59 +0800

    #19 Implemented persistence of events to event store

---
+++ activity-write-daemon/parsers/event.parser.go
@@ -0,0 +1,17 @@
+package parsers
+
+import (
+	"encoding/json"
+
+	"activity-write-daemon/models"
+)
+
+// ParseActivityJson method is used for parsing JSON string
+// to Activity struct instance.
+func ParseActivityJson(jsonString []byte) (*models.Activity, error) {
+	var activity *models.Activity
+	activityData := []byte(jsonString)
+	err := json.Unmarshal(activityData, &activity)
+
+	return activity, err
+}
---
+++ test-assets/README.md
@@ -0,0 +1,7 @@
+# Test Assets
+
+This folder contains all files relevant for testing this application.
+
+## Postman Testing
+
+To perform integration testing, Postman was heavily used during development. An export of the test cases we're located at `GO Social.postman_collection.json` file.
